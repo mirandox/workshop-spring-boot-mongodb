f24a74ff72e4ec1650f6e99c96028c9d38e22e02
Felipe Miranda <feguido1@gmail.com>
2019-09-11 15:34:44 -0300

    Conectando ao MongoDB com Repository e Service

---
+++ src/main/java/com/mirandox/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.mirandox.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mirandox.workshopmongo.domain.User;
+import com.mirandox.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+}
