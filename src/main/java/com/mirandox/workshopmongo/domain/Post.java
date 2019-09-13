5100cffff3724615094b82c39bfa4a6bf8a8e558
Felipe Miranda <feguido1@gmail.com>
2019-09-13 10:42:18 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/mirandox/workshopmongo/domain/Post.java
+++ src/main/java/com/mirandox/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.mirandox.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.mirandox.workshopmongo.dto.AuthorDTO;
+import com.mirandox.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -23,6 +26,8 @@ public class Post implements Serializable {
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {}
 
@@ -73,6 +78,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
